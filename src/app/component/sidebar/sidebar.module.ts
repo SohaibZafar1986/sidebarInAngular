f58fa8065b23fb90714135828d22d96ce8feb7be
unknown <sohaib@orenda.systems>
2022-08-23 15:32:33 +0500

    Delete icon module and sidebar components

--- src/app/component/sidebar/sidebar.module.ts
+++ src/app/component/sidebar/sidebar.module.ts
@@ -2,27 +2,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
-import { IconModule } from '../icon/icon.module';
+// import { IconModule } from '../icon/icon.module';
 
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { SidebarService } from './sidebar.service';
 import { SidebarBrandComponent } from './sidebar-brand/sidebar-brand.component';
-import { SidebarToggleDirective } from './sidebar-toggle/sidebar-toggle.directive';
-import { SidebarTogglerComponent } from './sidebar-toggler/sidebar-toggler.component';
-import { SidebarHeaderComponent } from './sidebar-header/sidebar-header.component';
-import { SidebarFooterComponent } from './sidebar-footer/sidebar-footer.component';
+// import { SidebarToggleDirective } from './sidebar-toggle/sidebar-toggle.directive';
+// import { SidebarTogglerComponent } from './sidebar-toggler/sidebar-toggler.component';
+// import { SidebarHeaderComponent } from './sidebar-header/sidebar-header.component';
+// import { SidebarFooterComponent } from './sidebar-footer/sidebar-footer.component';
 
 import { SharedModule } from '../shared';
 
 import {
   SidebarNavBadgePipe,
   SidebarNavComponent,
-  SidebarNavDividerComponent,
+  // SidebarNavDividerComponent,
   SidebarNavGroupComponent,
   SidebarNavHelper,
   SidebarNavIconPipe,
   SidebarNavItemClassPipe,
-  SidebarNavLabelComponent,
+  // SidebarNavLabelComponent,
   SidebarNavLinkComponent,
   SidebarNavLinkContentComponent,
   SidebarNavLinkPipe,
@@ -33,38 +33,38 @@ import {
 @NgModule({
   declarations: [
     SidebarComponent,
-    SidebarTogglerComponent,
-    SidebarToggleDirective,
+    // SidebarTogglerComponent,
+    // SidebarToggleDirective,
     SidebarBrandComponent,
     SidebarNavBadgePipe,
     SidebarNavComponent,
-    SidebarNavDividerComponent,
+    // SidebarNavDividerComponent,
     SidebarNavGroupComponent,
     // SidebarNavGroupToggleDirective,
     SidebarNavIconPipe,
     SidebarNavItemClassPipe,
-    SidebarNavLabelComponent,
+    // SidebarNavLabelComponent,
     SidebarNavLinkComponent,
     SidebarNavLinkContentComponent,
     SidebarNavLinkPipe,
     SidebarNavTitleComponent,
-    SidebarHeaderComponent,
-    SidebarFooterComponent,
+    // SidebarHeaderComponent,
+    // SidebarFooterComponent,
   ],
   imports: [
     CommonModule,
     RouterModule,
     SharedModule,
-    IconModule
+    // IconModule
   ],
   exports: [
     SidebarComponent,
-    SidebarToggleDirective,
-    SidebarTogglerComponent,
+    // SidebarToggleDirective,
+    // SidebarTogglerComponent,
     SidebarBrandComponent,
     SidebarNavComponent,
-    SidebarHeaderComponent,
-    SidebarFooterComponent,
+    // SidebarHeaderComponent,
+    // SidebarFooterComponent,
   ],
   providers: [
     SidebarService,
